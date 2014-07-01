fc7ffbae6bb221104e741b40587092f4018563ed
Michael Novotny <manovotny@manovotny-rmbp.local>
2014-06-30 20:40:18 -0500

    Bumps version for 0.1.1 release.

--- wp-ads.php
+++ wp-ads.php
@@ -12,7 +12,7 @@
  * Plugin Name: WP Ads
  * Plugin URI: https://github.com/manovotny/wp-ads
  * Description: Ads for WordPress.
- * Version: 0.1.0
+ * Version: 0.1.1
  * Author: Michael Novotny
  * Author URI: http://manovotny.com
  * License: GPL-3.0+
